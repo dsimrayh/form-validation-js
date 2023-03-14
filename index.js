e98539785c25c17c659d4503489858da68760641
Damian <dsimrayh@yahoo.com>
2023-03-13 18:27:07 -0700

    styling complete
    added basic CSS ststyling to the form + added header.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('test');
+const submit = document.querySelector('button[type="submit"');
+const inputs = document.querySelectorAll('input');
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('yo');
+});
+
+inputs.forEach((input) => {
+  console.log({ input: input.name, validity: input.validity });
+});
