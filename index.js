b198ae912ba30d617f9bd3c202c417dc296aeb0f
Damian <dsimrayh@yahoo.com>
2023-03-15 17:10:30 -0700

    minor updates
    added in error elements for each of the inputs and styled them. set up event listeners for each of the individual inputs.

--- index.js
+++ index.js
@@ -3,9 +3,35 @@ const inputs = document.querySelectorAll('input');
 
 submit.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log('yo');
+  console.log('form submit');
 });
 
 inputs.forEach((input) => {
   console.log({ input: input.name, validity: input.validity });
+});
+
+// Individual listeners
+const email = document.querySelector('#email');
+email.addEventListener('input', () => {
+  console.log('email updated');
+});
+
+const country = document.querySelector('#country');
+country.addEventListener('input', () => {
+  console.log('country updated');
+});
+
+const ZIP = document.querySelector('#ZIP');
+ZIP.addEventListener('input', () => {
+  console.log('ZIP updated');
+});
+
+const password = document.querySelector('#password');
+password.addEventListener('input', () => {
+  console.log('password updated');
+});
+
+const confirm = document.querySelector('#confirm');
+confirm.addEventListener('input', () => {
+  console.log('confirm updated');
 });
